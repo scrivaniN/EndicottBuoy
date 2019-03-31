2cb0186a485eda6642638fd45b9d962917d1861d
scrivaniN <31866373+scrivaniN@users.noreply.github.com>
2019-03-31 19:57:09 -0400

    Add files via upload
    added wind speed

--- Pi-Temp.py
+++ Pi-Temp.py
@@ -14,10 +14,58 @@ import time
 import schedule
 import glob
 import os
+import Adafruit_ADS1x15
 
 temperatureList = []
 humidityList = []
 waterList = []
+windList = []
+
+
+def readWindSpeed():
+  adc = Adafruit_ADS1x15.ADS1115()
+
+  GAIN = 1
+
+  print("Readng ADS1x15 values, press Ctrl-C to quit...")
+
+  print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+  print('-' * 37)
+  #Main loop
+  start = time.time()
+  #windList = []
+  counter = 0
+  average = 0
+  sensorVoltage = 0
+  windSpeed = 0
+  voltageConversionConstant = .00012523
+  voltageMin = 0.4
+  windSpeedMin = 0
+  voltageMax = 2.0
+  windSpeedMax = 32
+
+  while time.time() < start + 10:
+    values = [0]*4
+    for i in range(4):
+      values[i] = adc.read_adc(i, gain=GAIN)
+      print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+      average += values[0]
+      counter += 1
+      time.sleep(0.5)
+
+  average /= counter
+  sensorVoltage = average * voltageConversionConstant
+  if sensorVoltage <= voltageMin:
+    windSpeed = 0
+
+  else:
+    windSpeed = (sensorVoltage - voltageMin)*windSpeedMax/(voltageMax/voltageMin)*2.23694
+
+
+  windList.append(windSpeed)
+  #return windSpeed
+  print(average)
+  print(windSpeed)
 
 
 #specifies which sensor we are using(we are using dht22)
@@ -48,7 +96,7 @@ def read_temp_raw():
 def read_temp():
   lines = read_temp_raw() # Read the temperature 'device file'
  
-  # While the first line does not contain 'YES', wait for 0.2s
+  #While the first line does not contain 'YES', wait for 0.2s
   # and then read the device file again.
   while lines[0].strip()[-3:] != 'YES':
     time.sleep(0.2)
@@ -83,8 +131,11 @@ def readDHT22():
         sys.exit(1)
 
 def takeAvg(list):
-    avg = sum(list) / float(len(list))
-    return avg
+    
+    if len(list) > 0:
+      return sum(list) / float(len(list))
+    else:
+      return 0
 
 def writeToDB():
     print('writing to database' + 'Time' + str(now.hour) + ':' + str(now.minute))
@@ -92,8 +143,8 @@ def writeToDB():
     
     cnx = mysql.connector.connect(user = 'nick', password = 'Scribbles1$', host = 'ecbuoy.cl7cxw0gh9pq.us-east-2.rds.amazonaws.com', database = 'endicottbuoy')
     cursor = cnx.cursor()
-    query = ("INSERT INTO readings (temperature, humidity, date, water_temp) VALUES (%s, %s, %s, %s)")
-    dht_data = (takeAvg(temperatureList),takeAvg(humidityList), now, takeAvg(waterList))
+    query = ("INSERT INTO readings (temperature, humidity, date, water_temp, wind_speed) VALUES (%s, %s, %s, %s, %s)")
+    dht_data = (takeAvg(temperatureList),takeAvg(humidityList), now, takeAvg(waterList), takeAvg(windList))
     cursor.execute(query,dht_data)
 
     cnx.commit()
@@ -102,6 +153,7 @@ def writeToDB():
     temperatureList[:] = [] 
     humidityList[:] = []
     waterList[:] = []
+    windList[:] = []
     
     cursor.close()
     cnx.close()
@@ -110,17 +162,19 @@ def writeToDB():
     print('we have written to the database closing connection...')
 
 #tell the scheduler to write to the database every hour.
-schedule.every(5).minutes.do(writeToDB)
+schedule.every(1).minutes.do(writeToDB)
 
 while True:
     now = datetime.datetime.now()
-    print(read_temp())
-    humidity, temperature = readDHT22()
+    #print(read_temp())
+    #humidity, temperature = readDHT22()
+    readWindSpeed()
     print('reading values')
     sleep(sleepTime)
     #print('reading  humidity list values')
     print humidityList
     print waterList
+    print windList
     
     #if there is a schedule pending run the task.
     schedule.run_pending()
